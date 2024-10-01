b14bc6965266966f28859359e241b20c3c3c98cd
Kukuh Wicaksono <kukuhwicaksono105xb@gmail.com>
2024-10-01 18:26:32 +0700

    Tugas 6 - reactJS

---
+++ tugas-reacjs/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="container">
+          <h1>Hi, Kukuh Wicaksono</h1>
+          <hr></hr>
+          <small>Saya dari Batch 60 sudah berhasil menginstall React</small>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
